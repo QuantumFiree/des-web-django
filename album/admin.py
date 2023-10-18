8c694ce7bd9f682d17e13805215a277e6a52bb2b
Udenar <arteaga980910@gmail.com>
2023-10-18 17:44:49 -0500

    django-project

---
+++ album/admin.py
@@ -0,0 +1,7 @@
+# Register your models here.
+from django.contrib import admin
+from album.models import Selection, Player
+
+# Register your models here.
+admin.site.register(Selection)
+admin.site.register(Player)
